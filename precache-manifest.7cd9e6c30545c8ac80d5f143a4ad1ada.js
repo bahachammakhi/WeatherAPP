bf1cff218526a44b44e928413c529a89376ee248
Baha chammakhi <bahachammakhi25@gmail.com>
2019-07-12 23:46:56 +0100

    Updates

---
+++ precache-manifest.7cd9e6c30545c8ac80d5f143a4ad1ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333fe56dad019e11cf05c7107d4856df",
+    "url": "/WeatherAPP/index.html"
+  },
+  {
+    "revision": "f0f248ca5c6421e0a3f4",
+    "url": "/WeatherAPP/static/css/main.8689721d.chunk.css"
+  },
+  {
+    "revision": "1bb656d6c4e0e20f9a26",
+    "url": "/WeatherAPP/static/js/2.508f0c82.chunk.js"
+  },
+  {
+    "revision": "f0f248ca5c6421e0a3f4",
+    "url": "/WeatherAPP/static/js/main.70e42c2f.chunk.js"
+  },
+  {
+    "revision": "b7cbf642289a7f083cb0",
+    "url": "/WeatherAPP/static/js/runtime~main.1c1f8e3f.js"
+  },
+  {
+    "revision": "e24d74155508d9e7c73bd2b431684882",
+    "url": "/WeatherAPP/static/media/Cloudysunny.e24d7415.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/WeatherAPP/precache-manifest.7cd9e6c30545c8ac80d5f143a4ad1ada.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WeatherAPP/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
